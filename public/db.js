5b97d5404fdceb36edb62aa0c33845c771b36443
Ashly Mayle <infiernodeashly@gmail.com>
2021-01-18 13:03:53 -0500

    updates

--- public/db.js
+++ public/db.js
@@ -1,45 +1,53 @@
 let db;
-// create a new db request for a "budget" database.
+// Open database. Start at version 1. 
 const request = indexedDB.open("budget", 1);
 
+//---
+//Loading and checking database for data. 
 request.onupgradeneeded = function (event) {
   // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
-
+//begins what to do with the results upon successful completion.
 request.onsuccess = function (event) {
   db = event.target.result;
 
-  // check if app is online before reading from db
+  // if app online, read data from the database. checkDatabase function outlined below.
   if (navigator.onLine) {
     checkDatabase();
   }
 };
-
+//if error, return "Uh oh! Please google + error code"
 request.onerror = function (event) {
-  console.log("Woops! " + event.target.errorCode);
+  console.log("Uh oh! Please google " + event.target.errorCode);
 };
+//---
 
+//---
+//creates function for saving a record. 
 function saveRecord(record) {
   // create a transaction on the pending db with readwrite access
   const transaction = db.transaction(["pending"], "readwrite");
-
   // access your pending object store
   const store = transaction.objectStore("pending");
 
-  // add record to your store with add method.
+  // add record to store.
   store.add(record);
 }
+//---
 
+//---
+//checkDatabase method referenced upon loading.
 function checkDatabase() {
-  // open a transaction on your pending db
+  // open a read/write transaction on your pending db
   const transaction = db.transaction(["pending"], "readwrite");
   // access your pending object store
   const store = transaction.objectStore("pending");
   // get all records from store and set to a variable
   const getAll = store.getAll();
-
+  // when the system successfully pulls the results, if there is at least one item returned, fet the listed link. Then send the results 
+  //as a JSON object to the listed link. If successful, open a transaction on the database, access the pending object store and clear items in store.
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
       fetch("/api/transaction/bulk", {
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,4 @@
+// precaching layouts and data for offline work. 
 const PreCacheFiles = [
   "/",
   "/index.html",
@@ -7,27 +8,35 @@ const PreCacheFiles = [
   "/manifest.webmanifest",
 ];
 
-
+// defining caches for layout and data
 const CACHE_NAME = "static-cache-v14";
 const DATA_CACHE_NAME = "data-cache-v13";
 
+//---
 // install
 self.addEventListener("install", function (evt) {
+  //waiting on the install until the files are successfully precached. 
   evt.waitUntil(
+    //Open layout cache and send message to console that it was precached successfully. 
     caches.open(CACHE_NAME).then(cache => {
-      console.log("Your files were pre-cached successfully!");
+      console.log("Your files were precached successfully! Whoohoo!");
+      //Returned all precached files from above. 
       return cache.addAll(PreCacheFiles);
     })
   );
-
+    //installed service worker skips waiting and moves directly to activating status so it doesn't get stuck waiting forever to become active. 
   self.skipWaiting();
 });
+//---
 
 // activate
 self.addEventListener("activate", function (evt) {
+  //wait to activate until all old unused caches are cleaned up. If there is cached data still available that is no longer available in the system
+  //the cached data will be removed via it's key. 
   evt.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
+        //if the cached data and layout do not equal what is still isted in the keyList, then it will be removed from the cached lists.
         keyList.map(key => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Removing old cache data", key);
@@ -41,7 +50,7 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-// fetch
+// fetch data from API and store in cache if it is an item the user finds or if user pulls all data. 
 self.addEventListener("fetch", function (evt) {
   const { url } = evt.request;
   if (url.includes("/all") || url.includes("/find")) {
@@ -62,8 +71,10 @@ self.addEventListener("fetch", function (evt) {
           });
       }).catch(err => console.log(err))
     );
+
+    //
   } else {
-    // respond from static cache, request is not for /api/*
+    // respond from static cache, request is not for /api/* This sets up your personal response for the promise
     evt.respondWith(
       caches.open(CACHE_NAME).then(cache => {
         return cache.match(evt.request).then(response => {
